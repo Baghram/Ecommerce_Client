b3af73200e54dcbf3e8be13f50b217652b9c0d24
Yoko <baghram0001@gmail.com>
2020-04-15 09:50:28 +0700

    store updated

--- ecommerce-admin-client/src/store/index.js
+++ ecommerce-admin-client/src/store/index.js
@@ -1,14 +1,104 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    IsLogin: false,
+    IsAdmin: false,
+    Products: [],
+    Cart: [],
+    Orders: []
   },
   mutations: {
+    SET_LOGIN (state, payload) {
+      state.IsLogin = payload
+    },
+    SET_ADMIN (state, payload) {
+      state.IsAdmin = payload
+    },
+    SET_PRODUCTS (state, payload) {
+      state.Products = payload
+    }
   },
   actions: {
+    Login (context, payload) {
+      return axios({
+        url: 'http://localhost:3000/user/login',
+        method: 'POST',
+        data: {
+          Email: payload.email,
+          Password: payload.password
+        }
+      })
+    },
+    Register (context, payload) {
+      return axios({
+        url: 'http://localhost:3000/user/register',
+        method: 'POST',
+        data: {
+          Email: payload.email,
+          Password: payload.password
+        }
+      })
+    },
+    FetchData ({ commit }) {
+      axios({
+        url: 'http://localhost:3000/product/',
+        method: 'GET',
+        headers: {
+          access_token: localStorage.getItem('access_token')
+        }
+      })
+    },
+    AddProduct (context, payload) {
+      return axios({
+        url: 'http://localhost:3000/product/',
+        method: 'POST',
+        headers: {
+          access_token: localStorage.getItem('access_token')
+        }
+      })
+    },
+    UpdateProduct (context, payload) {
+      return axios({
+        url: `http://localhost:3000/product/${payload.id}`,
+        method: 'PATCH',
+        headers: {
+          access_token: localStorage.getItem('access_token')
+        },
+        data: {
+          Name: payload.Name,
+          Image_Url: payload.Image_Url,
+          Stock: payload.Stock,
+          Price: payload.Price
+        }
+      })
+    },
+    DeleteProduct (context, payload) {
+      return axios({
+        url: `http://localhost:3000/product/${payload.id}`,
+        method: 'DELETE',
+        headers: {
+          access_token: localStorage.getItem('access_token')
+        }
+      })
+    },
+    FetchDetail (context, payload) {
+      return axios({
+        url: `http://localhost:3000/product/${payload.id}`,
+        method: 'GET',
+        headers: {
+          access_token: localStorage.getItem('access_token')
+        }
+      })
+    },
+    Logout ({ commit }) {
+      commit('SET_LOGIN', false)
+      commit('SET_ADMIN', false)
+    }
   },
   modules: {
   }
--- ecommerce-admin-client/src/views/Home.vue
+++ ecommerce-admin-client/src/views/Home.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="home">
+    <h1>Welcome to The Store</h1>
     <img alt="logo" src="../assets/logo.png">
     <h1>One Stop Solution for your needs</h1>
     <p>This is the Admin Pages for Adding, Update and removing Products.</p>
+    <p>Press the button on top left for menu</p>
+    <p>this is admin area,not client</p>
   </div>
 </template>
 
@@ -18,5 +21,6 @@ export default {
 <style >
 .home{
   text-align: center;
+  margin-top: 200px;
 }
 </style>
